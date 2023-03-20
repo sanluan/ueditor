b84962859cbff2a3a8bb9e8bfdfe3672edc442f9
sanluan <master@publiccms.com>
2023-03-20 10:32:46 +0800

    删除多余的空行

--- ueditor.parse.js
+++ ueditor.parse.js
@@ -9,57 +9,39 @@
         ];
 
     function getUEBasePath ( docUrl, confUrl ) {
-
         return getBasePath( docUrl || self.document.URL || self.location.href, confUrl || getConfigFilePath() );
-
     }
 
     function getConfigFilePath () {
-
         var configPath = document.getElementsByTagName('script');
-
         return configPath[ configPath.length -1 ].src;
 
     }
 
     function getBasePath ( docUrl, confUrl ) {
-
         var basePath = confUrl;
-
         if ( !/^[a-z]+:/i.test( confUrl ) ) {
-
             docUrl = docUrl.split( "#" )[0].split( "?" )[0].replace( /[^\\\/]+$/, '' );
-
             basePath = docUrl + "" + confUrl;
-
         }
-
         return optimizationPath( basePath );
 
     }
 
     function optimizationPath ( path ) {
-
         var protocol = /^[a-z]+:\/\//.exec( path )[ 0 ],
             tmp = null,
             res = [];
-
         path = path.replace( protocol, "" ).split( "?" )[0].split( "#" )[0];
-
         path = path.replace( /\\/g, '/').split( /\// );
-
         path[ path.length - 1 ] = "";
-
         while ( path.length ) {
-
             if ( ( tmp = path.shift() ) === ".." ) {
                 res.pop();
             } else if ( tmp !== "." ) {
                 res.push( tmp );
             }
-
         }
-
         return protocol + res.join( "/" );
 
     }
